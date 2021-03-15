467609d81710a1097b5daf108752baf02c85b570
XY <chelciedealmeida@gmail.com>
2021-03-14 23:59:24 -0700

    added answer validation that would display if chosen answer is correct or incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,15 @@ var choiceButton2 = document.getElementById('choiceButton2');
 var choiceButton3 = document.getElementById('choiceButton3');
 var choiceButton4 = document.getElementById('choiceButton4');
 var choiceButtons = document.getElementsByClassName('choiceButton');
+var correctEl = document.getElementById('correct');
+var incorrectEl = document.getElementById('incorrect');
 var initialQuestion = 0;
 
 quizQuestionsEl.style.display = 'none';
 quizSubmissionEl.style.display = 'none';
 highScoresEl.style.display = 'none';
+correctEl.style.display = 'none';
+incorrectEl.style.display = 'none';
 
 var qsetArray = [
     {
@@ -30,7 +34,7 @@ var qsetArray = [
         opt2: 'Function and dynamics',
         opt3: 'It rules CSS and HTML',
         opt4: "'it's a browser database'",
-        corAns: '2'
+        corAns: '3'
     }
 ];
 
@@ -49,7 +53,39 @@ var quizStart = function() {
     choiceButton4.innerText=qsetArray[initialQuestion].opt4;
 };
 
-// adding event listener to ensure question change occurs when 
+// adding event listener to ensure question change occurs when the user clicks on an option
+
     for (i = 0; i < choiceButtons.length; i++) {
-        console.log('Hello')
+        var choiceButton = choiceButtons[i]
+        choiceButton.addEventListener('click',function handleclick(event) {
+        // the choice is evaluated for correctness
+        var clickedButton = event.target
+        var chosenAnswer = clickedButton.getAttribute('data-value');
+        var correctAnswer = qsetArray[initialQuestion].corAns;
+        var wereTheyCorrect = chosenAnswer == correctAnswer;
+        // console.log(wereTheyCorrect);
+        
+        // I want to verify if the chosen answer is equal to the correct answer.
+        // if so, we want to display correct. Else, we want to display incorrect
+
+        if (chosenAnswer == correctAnswer) {
+
+           correctEl.style.display = 'block';
+        }
+        else {
+            incorrectEl.style.display = 'block'; 
+        }
+        
+        setTimeout(function() {
+
+            correctEl.style.display = 'none'
+            incorrectEl.style.display = 'none'
+            initialQuestion +=1   
+            quizStart();    
+        }, 1000);
+        // initialQuestion +=1   
+        // quizStart();
+
+        })
     };
+
